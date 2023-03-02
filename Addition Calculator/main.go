21aed929db31573b741c1007aabfe355630d0de0
Terry Miller <temiller@linkedin.biz>
2023-03-02 08:58:35 -0800

    Initial Commit

---
+++ Addition Calculator/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	//Get the first value
+	fmt.Print("Value 1: ")
+	input1, _ := reader.ReadString('\n')
+	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 1 must be a number")
+	}
+
+	//Get the second value
+	fmt.Print("Value 2: ")
+	input2, _ := reader.ReadString('\n')
+	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 2 must be a number")
+	}
+
+	//Compute the sum
+	sum := float1 + float2
+	sum = math.Round(sum*100) / 100
+	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+
+}
---
+++ Switch's/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "It's Sunday!"
+	case 2:
+		result = "It's Monday!"
+	default:
+		result = "It's some other day!"
+	}
+	fmt.Println(result)
+}
