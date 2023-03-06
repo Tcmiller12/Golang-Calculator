e6bbc9868cc30c9c6607900948f5c09b83da6f7b
Terry Miller <temiller@linkedin.bit>
2023-03-06 09:50:01 -0800

    Added feature to eliminate text from serial number in window's CMD

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/temiller/calculator
+
+go 1.20
--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 )
 
 func main() {
+	// parse the given serial number
+	result := parseSerialNumber("SerialNumber=018277314057SerialNumber=")
+	fmt.Println("the serial number is", result)
+	return 
+	
 	reader := bufio.NewReader(os.Stdin)
 	//Gives value 1
 	fmt.Print("Value 1: ")
@@ -31,5 +36,15 @@ func main() {
 	sum := float1 + float2
 	sum = math.Round(sum*100) / 100
 	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+}
 
+func parseSerialNumber(input string) string {
+	result := strings.Replace(input, "SerialNumber=", "", 1)
+	// append to the result the hostname
+	hostname, err := os.Hostname()
+	if err != nil {
+		return result
+	}
+	result += "-" + hostname
+	return result
 }
